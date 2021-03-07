7e456d57f2bc7a509ac3ee4bfe258f816253cf7d
olopcodes <olopcodes@gmail.com>
2021-03-06 21:18:55 -0500

    added function file and local storage

--- js/app.js
+++ js/app.js
@@ -1,80 +1,55 @@
+"use strict";
 const mainListForm = document.querySelector("#main-list__form");
-const taskDetailsForm = document.querySelector("#task-details__form");
+const taskListForm = document.querySelector("#task-list__form");
 const mainListContainer = document.querySelector(".main-list__container");
-const taskDetailsContainer = document.querySelector(".task-details__container");
-const taskHeading = document.querySelector(".task-details__heading");
-const taskLeftSpan = document.querySelector(".task-left");
-let data = [];
+const taskListHeading = document.querySelector(".task-list__heading h2");
+const taskListHeadingSpan = document.querySelector(".task-list__heading span");
+const taskListContainer = document.querySelector(".task-list__container");
+const taskListBtns = document.querySelector(".task-list__btns");
+const data = getSavedData();
 
-// functions
+// events
+window.addEventListener("load", (e) => {
+  if (data.length > 0) showMainTitle();
+});
 
-const findDuplicates = (arr, text) => {
-  return arr.findIndex(
-    (task) => task.name.toLowerCase() === text.toLowerCase()
-  );
-};
-
-const findTask = (text) => {
-  return data.find((item) => item.name.toLowerCase() === text.toLowerCase());
-};
-
-const populateTaskDetails = (taskObj) => {
-  // setting the data attribute, to target it later on
-  taskHeading.setAttribute("data-name", taskObj.name);
-  if (!taskObj.tasks)
-    taskHeading.innerHTML = `<h2 class="task-details__heading">
-  ${taskObj.name} <span>0 Tasks Left</span>`;
-};
-
-const createMainTaskElement = (text) => {
-  const li = document.createElement("li");
-  li.textContent = text;
-  li.addEventListener("click", (e) => {
-    // find the corresponding task
-    const task = findTask(e.target.textContent);
-    // show the task details to the user
-    populateTaskDetails(task);
-  });
-  mainListContainer.appendChild(li);
-};
-
-const taskDetailsHTML = (text) => {
-  return `<label>
-    <input type="checkbox" id="hide-complete" />
-    ${text}
-  </label>`;
-};
-
-const getTasksLeft = (arr) => {
-  return arr.filter((item) => !item.comlpleted);
-};
-
-const render = (type) => {
-  if (type === "main") {
-    mainListContainer.innerHTML = "";
-    data.forEach((item) => createMainTaskElement(item.name));
-  } else if (type === "tasks") {
-  }
-};
-
-// event listeners
 mainListForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const type = "main";
-  const newTask = e.target.elements.mainText.value;
-  const duplicate = findDuplicates(data, newTask);
-  duplicate === -1
-    ? data.push({ name: newTask, tasks: [] })
-    : alert("no duplicates!");
-  render(type);
+  const newMainTask = e.target.elements.mainText.value;
+  const isValidText = inputCheck(newMainTask);
+  const titleFound = findTitle(newMainTask);
+  if (titleFound) return;
+  if (isValidText) {
+    data.push({ title: newMainTask, tasks: [] });
+    saveData(data);
+    createMainTaskElement(newMainTask);
+  }
   e.target.elements.mainText.value = "";
 });
 
-taskDetailsForm.addEventListener("submit", (e) => {
+taskListForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const type = "tasks";
-  const task = e.target.elements.detailsText.value;
-  const mainTask = findTask(taskHeading.getAttribute("data-name"));
-  const duplicate = findDuplicates(mainTask.tasks);
-  if (duplicate === -1) mainTask.tasks.push({ text: task, comlpleted: false });
+  const newListTask = e.target.elements.listText.value;
+  const title = findTitle(taskListHeading.textContent);
+  const isValidText = inputCheck(newListTask);
+  const hasTask =
+    title.tasks.length > 0 ? findTask(title.title, newListTask) : false;
+  if (hasTask) return;
+  if (isValidText) title.tasks.push({ text: newListTask, completed: false });
+  saveData(data);
+  populateTaskInfo(title);
+  e.target.elements.listText.value = "";
+});
+
+taskListContainer.addEventListener("change", (e) => {
+  const title = taskListHeading.textContent;
+  const textTask = e.target.closest("label").textContent.trim();
+  const task = findTask(title, textTask);
+  if (task) task.completed = !task.completed;
+  taskListHeadingSpan.textContent = getTasksLeft(findTitle(title));
+});
+
+taskListBtns.addEventListener("click", (e) => {
+  if (e.target.id === "delete") deleteList();
+  if (e.target.id === "reset") resetCompleted();
 });
---
+++ js/task-functions.js
@@ -0,0 +1,100 @@
+// functions
+const getSavedData = () => {
+  // checking if saved data
+  const notesJSON = localStorage.getItem("data");
+  try {
+    return notesJSON ? JSON.parse(notesJSON) : [];
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveData = (data) => {
+  localStorage.setItem("data", JSON.stringify(data));
+};
+
+const findTitle = (text) => {
+  // { title: newMainTask, tasks: [] }
+  return data.find((title) => title.title === text);
+};
+
+const findTask = (mainTitle, textTask) => {
+  // { title: newMainTask, tasks: [] }
+  const title = findTitle(mainTitle);
+  // if undefined, means nothing in there
+  return title.tasks.find((task) => task.text === textTask);
+};
+
+const inputCheck = (text) => {
+  // is the text a string and at least 3 characters
+  if (isNaN(text) && text.length >= 3) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const createMainTaskElement = (text) => {
+  const li = document.createElement("li");
+  li.textContent = text;
+  li.addEventListener("click", (e) => {
+    // use the info from here to populate the tasks
+    populateTaskInfo(findTitle(li.textContent));
+  });
+  mainListContainer.appendChild(li);
+};
+
+const getTasksLeft = (title) => {
+  const tasksLeft = title.tasks.filter((task) => !task.completed).length;
+  return `${tasksLeft} Tasks Left`;
+};
+
+const taskDetailsHTML = (text) => {
+  return `<label>
+      <input type="checkbox" />
+      ${text}
+    </label>`;
+};
+
+const handleTaskInfo = (title) => {
+  taskListHeadingSpan.textContent = getTasksLeft(title);
+  taskListContainer.innerHTML = "";
+  title.tasks.forEach(
+    (task) => (taskListContainer.innerHTML += taskDetailsHTML(task.text))
+  );
+};
+
+const populateTaskInfo = (title) => {
+  taskListContainer.innerHTML = "";
+  taskListHeading.textContent = title.title;
+  if (title.tasks.length === 0) {
+    taskListHeadingSpan.textContent = "0 Tasks Left";
+  } else if (title.tasks.length > 0) {
+    handleTaskInfo(title);
+  }
+};
+
+const showMainTitle = () => {
+  mainListContainer.innerHTML = "";
+  data.forEach((title) => createMainTaskElement(title.title));
+};
+
+const deleteList = () => {
+  const index = data.findIndex(
+    (title) => title.title === taskListHeading.textContent
+  );
+  data.splice(index, 1);
+  showMainTitle();
+};
+
+const resetCompleted = () => {
+  const title = findTitle(taskListHeading.textContent);
+  title.tasks.forEach((task) => (task.completed = false));
+  taskListHeadingSpan.textContent = getTasksLeft(title);
+  removeChecked();
+};
+
+const removeChecked = () => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach((check) => (check.checked = false));
+};
